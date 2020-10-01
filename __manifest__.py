0279e47e53cced3805a7134f79a2cc42ae6adad0
matjaz15 <matjo15@hotmail.com>
2020-10-01 17:34:22 +0200

    First commit of my odoo module

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Activity Calendar Title Link',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Adds a title link to an activity form when viewing it form the calendar dashboard view that links to its origin',
+    'sequence': '10',
+    'author':'Matjaž Brilj',
+    'depends': ['mail'],
+    'data': [ 'views/activity_popup_view.xml' ],
+    'demo': [ ],
+    'installable': True,
+    'application': True,
+    'qweb': [ ],
+}
